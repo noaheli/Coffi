f65253766997f8afb489763d205a5333b8c97b2a
Blake Williams <blakewilliams@Blakes-MacBook-Pro-2.local>
2020-04-22 08:42:09 -0400

    outlined a file structure for future development

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './src/pages/Home';
+import Capture from './src/pages/Capture';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Home/>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
