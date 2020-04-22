6bbd39f1589616ed88b5cd391b8e0ac3b4f82fa2
Blake Williams <blakewilliams@Blakes-MacBook-Pro-2.local>
2020-04-22 10:04:06 -0400

    linked the toggleDrawer to the hamburger button

--- App.js
+++ App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/pages/Home';
 import Capture from './src/pages/Capture';
-import TopBar from './src/components/TopBar'
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import {NavigationContainer} from '@react-navigation/native';
 
@@ -12,12 +11,14 @@ export default class App extends Component {
 
   render() {
     return (
-      <NavigationContainer>
-        <Drawer.Navigator initialRouteName="Home">
-          <Drawer.Screen name="Home" component={Home}/>
-          <Drawer.Screen name="Capture" component={Capture}/>
-        </Drawer.Navigator>
-      </NavigationContainer>
+      <View style={styles.container}>
+        <NavigationContainer>
+          <Drawer.Navigator initialRouteName="Home">
+            <Drawer.Screen name="Home" component={Home}/>
+            <Drawer.Screen name="Capture" component={Capture}/>
+          </Drawer.Navigator>
+        </NavigationContainer>
+      </View>
     );
   }
 }
